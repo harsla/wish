8d9a90bcfb9da2670c904a1f558aaf65c9f1394f
KyleCartmell <kylecartmell@gmail.com>
2013-11-20 22:54:05 -0700

    basic request handler composition

---
+++ test/unit/functional/http.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var niceTests = require("../../utils/nice-tests.js"),
+  constants = require("../../../lib/constants.js"),
+  wish = require("../../../lib/wish.js");
+
+suite('functional-http', function () {
+
+  var TEST_PATH = "/foo";
+
+  test('basic service', niceTests.createAndTestService(function (app) {
+    app.get(TEST_PATH, wish.composeContextualizedRequestHandler(wish.respondOk));
+  },
+    {
+      request: {
+        method: constants.HTTP_GET,
+        path: TEST_PATH
+      },
+      response: {
+        statusCode: constants.HTTP_OK
+      }
+    }));
+
+});
--- test/utils/http-client.js
+++ test/utils/http-client.js
@@ -1,1 +1,29 @@
+"use strict";
+var _ = require("underscore"),
+  http = require("http"),
+  http_client = module.exports;
+
+http_client.request = function (method, host, port, path, callback) {
+  var request = http.request({ method: method, hostname: host, port: port, path: path },
+    function (response) {
+      var friendlyResponse = {
+        statusCode: response.statusCode,
+        payload: ""
+      };
+      response.setEncoding("utf8");
+      response.on("data", function (data) {
+        friendlyResponse.payload += data;
+      });
+      response.on("end", function () {
+        callback(friendlyResponse);
+      });
+    });
+
+  request.on("error", function (error) {
+    console.log("error!!", error);
+  });
+
+  request.end();
+};
+
 
