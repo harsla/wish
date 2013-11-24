53d74d407fcf3f3cd30df0978b4a736a8f58dd5e
KyleCartmell <kylecartmell@gmail.com>
2013-11-23 17:12:12 -0700

    processor and responder short circuiting

--- test/unit/functional/http.js
+++ test/unit/functional/http.js
@@ -4,7 +4,7 @@ var niceTests = require("../../utils/nice-tests.js"),
   constants = require("../../../lib/constants.js"),
   wish = require("../../../lib/wish.js");
 
-wish.log.level = constants.LOG_WARN;
+wish.log.level = constants.LOG_DEBUG;
 
 suite('functional-http', function () {
 
@@ -131,6 +131,54 @@ suite('functional-http', function () {
       },
       response: {
         statusCode: constants.HTTP_NOT_FOUND,
+        payload: ""
+      }
+    }));
+
+  test('short circuit processors', niceTests.createAndTestService(function (app) {
+      app.get(TEST_PATH, wish.composeContextualizedRequestHandler(wish.generateContextResponder(),
+        function (context, callback) {
+          context.x = 3;
+          callback(constants.SHORT_CIRCUIT_PROCESSORS, context);
+        },
+        function (context, callback) {
+          context.y = 5;
+          callback(undefined, context);
+        }));
+    },
+    {
+      request: {
+        method: constants.HTTP_GET,
+        path: TEST_PATH
+      },
+      response: {
+        statusCode: constants.HTTP_OK,
+        payload: {
+          x: 3
+        }
+      }
+    }));
+
+  test('short circuit processors and responders', niceTests.createAndTestService(function (app) {
+    app.get(TEST_PATH, wish.composeContextualizedRequestHandler(wish.generateOkResponder(),
+      function (context, callback) {
+        context.x = 3;
+        context.internal.response.statusCode = constants.HTTP_UNAUTHORIZED;
+        context.internal.response.end();
+        callback(constants.SHORT_CIRCUIT_PROCESSORS, context);
+      },
+      function (context, callback) {
+        context.y = 5;
+        callback(undefined, context);
+      }));
+  },
+    {
+      request: {
+        method: constants.HTTP_GET,
+        path: TEST_PATH
+      },
+      response: {
+        statusCode: constants.HTTP_UNAUTHORIZED,
         payload: ""
       }
     }));
