23617046444833cf3d50722001b8fdacc8c2595b
KyleCartmell <kylecartmell@gmail.com>
2013-12-09 17:33:25 -0700

    input constraint violation handling

--- test/unit/functional/input.js
+++ test/unit/functional/input.js
@@ -96,4 +96,31 @@ suite('functional-input', function () {
       }
     }));
 
+  test('constraint violation => HTTP 400 bad request', niceTests.createAndTestService(function (app) {
+    app.post(TEST_PATH, wish.composeContextualizedRequestHandler(wish.generateOkResponder(),
+      wish.acceptInput({
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+          "bar": {
+            "type": "string",
+            "maxLength": 1
+          }
+        }
+      }),
+      function (context, callback) {
+        context.internal.input.bar.should.equal("baz");
+        callback(undefined, context);
+      }));
+  },
+    {
+      request: {
+        method: constants.HTTP_POST,
+        path: TEST_PATH + "?bar=baz"
+      },
+      response: {
+        statusCode: constants.HTTP_BAD_REQUEST
+      }
+    }));
+
 });
