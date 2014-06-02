e54bd1637bf41280bd620b1a351a520470fd1aaf
KyleCartmell <kylecartmell@gmail.com>
2014-06-02 13:21:17 -0700

    implemented and refactored some workflow unit tests

---
+++ test/integration/level-one/rdbms.js
@@ -0,0 +1,66 @@
+"use strict";
+
+var _ = require("underscore"),
+  niceTests = require("../utils/nice-tests.js"),
+  constants = require("../../lib/constants.js"),
+  wish = require("../../lib/wish.js");
+
+wish.log.level = constants.LOG_WARN;
+
+suite('rdbms-integration', function () {
+
+  var TEST_PATH = "/foo";
+
+  test('select 1', function (done) {
+    wish.configureDatabase(wish.mysql.configure("mysql", "mysql://wish:wish@localhost/wish"));
+    wish.composeContextualizedRequestHandler(wish.generateContextResponder(),
+      wish.connectToDatabase("mysql"),
+      function (context, callback) {
+        Object.keys(context.internal.databaseConnections).length.should.equal(1);
+        Object.keys(context.internal.databaseConnections).indexOf("mysql").should.equal(0);
+        callback(undefined, context);
+      },
+      wish.sql("mysql", "SELECT ? AS foo", [1], function (context, resultSet, callback) {
+        context.foo = resultSet[0].foo;
+        callback();
+      }),
+      wish.disconnectFromDatabase("mysql"),
+      function (context, callback) {
+        Object.keys(context.internal.databaseConnections).length.should.equal(0);
+        context.foo.should.equal(1);
+        callback(undefined, context);
+      }
+      )({}, {write: function () {},
+      end: function () {
+        this.statusCode.should.equal(200);
+        done();
+      }});
+  });
+
+  test('erroneous query', function (done) {
+    wish.configureDatabase(wish.mysql.configure("mysql", "mysql://wish:wish@localhost/wish"));
+    wish.composeContextualizedRequestHandler(wish.generateContextResponder(),
+        wish.connectToDatabase("mysql"),
+        wish.sql("mysql", "TCELES STAR FROM thingses WHERE roffel", [], function (context, resultSet, callback) {
+        callback();
+      }),
+      wish.disconnectFromDatabase("mysql")
+      )({}, {write: function () {},
+      end: function () {
+        this.statusCode.should.equal(500);
+        done();
+      }});
+  });
+
+  test('nonexistant database', function (done) {
+    wish.configureDatabase(wish.mysql.configure("mysql", "mysql://wish:wish@localhost/does_not_exist"));
+    wish.composeContextualizedRequestHandler(wish.generateContextResponder(),
+            wish.connectToDatabase("mysql")
+        )({}, {write: function () {},
+      end: function () {
+        this.statusCode.should.equal(500);
+        done();
+      }});
+  });
+
+});
--- test/unit/level-1/request.js
+++ test/unit/level-1/request.js
@@ -10,25 +10,30 @@ wish.log.level = constants.LOG_WARN;
 suite('request-control-flow', function () {
 
   test('normal flow', function (done) {
-    var x = 0;
+    // let
+    var x = 0,
+      assertMilestone = function (expectedMilestone) {
+        x += 1;
+        x.should.equal(expectedMilestone);
+      }, milestoneStep = function (expectedMilestone) {
+        return function (context, callback) {
+          assertMilestone(expectedMilestone);
+          callback(undefined, context);
+        }
+      },
+      milestoneFinalizer = function (milestone) {
+        assertMilestone(milestone);
+          done();
+      };
+    // given, expect
     wish.composeContextualizedRequestHandler(
       function responder (context) {
-        x += 1;
-        x.should.equal(4);
+        assertMilestone(4);
         done();
-      }, function stepOne (context, callback) {
-        x += 1;
-        x.should.equal(1);
-        callback(undefined, context);
-      }, function stepTwo (context, callback) {
-        x += 1;
-        x.should.equal(2);
-        callback(undefined, context);
-      }, function stepThree (context, callback) {
-        x += 1;
-        x.should.equal(3);
-        callback(undefined, context);
-      })({}, {});
+      },
+      milestoneStep(1),
+      milestoneStep(2),
+      milestoneStep(3))({}, {});
   });
 
   test('faulty flow', function (done) {
@@ -83,6 +88,81 @@ suite('request-control-flow', function () {
       }, function stepThree (context, callback) {
         assert.fail();
       })({}, {write:_.constant(), end:_.constant()});
+  });
+
+  test('normal flow with finalizers', function (done) {
+    var x = 0;
+    wish.composeContextualizedRequestHandler(
+      function responder (context) {
+        x += 1;
+        x.should.equal(4);
+      }, wish.buildStep(function stepOne (context, callback) {
+        x += 1;
+        x.should.equal(1);
+        callback(undefined, context);
+      }, function finalizerOne (context, callback) {
+        x += 1;
+        x.should.equal(7);
+        done();
+        callback(undefined, context);
+      }), wish.buildStep(function stepTwo (context, callback) {
+        x += 1;
+        x.should.equal(2);
+        callback(undefined, context);
+      }, function finalizerTwo (context, callback) {
+        x += 1;
+        x.should.equal(6);
+        callback(undefined, context);
+      }), wish.buildStep(function stepThree (context, callback) {
+        x += 1;
+        x.should.equal(3);
+        callback(undefined, context);
+      }, function finalizerThree (context, callback) {
+        x += 1;
+        x.should.equal(5);
+        callback(undefined, context);
+      }))({}, {});
+  });
+
+  test('faulty flow with finalizers', function (done) {
+    var x = 0;
+    wish._generalErrorHandler = wish.generalErrorHandler;
+    wish.generalErrorHandler = function (error, context) {
+      x += 1;
+      x.should.equal(4);
+      wish.generalErrorHandler = wish._generalErrorHandler;
+      delete wish._generalErrorHandler;
+      wish.generalErrorHandler(error, context);
+    }
+    wish.composeContextualizedRequestHandler(
+        function responder (context) {
+          assert.fail();
+        }, wish.buildStep(function stepOne (context, callback) {
+          x += 1;
+          x.should.equal(1);
+          callback(undefined, context);
+        }, function finalizerOne (context, callback) {
+          x += 1;
+          x.should.equal(7);
+          done();
+          callback(undefined, context);
+        }), wish.buildStep(function stepTwo (context, callback) {
+          x += 1;
+          x.should.equal(2);
+          callback(undefined, context);
+        }, function finalizerTwo (context, callback) {
+          x += 1;
+          x.should.equal(6);
+          callback(undefined, context);
+        }), wish.buildStep(function stepThree (context, callback) {
+          x += 1;
+          x.should.equal(3);
+          callback(wish.generateError(500,"boop"), context);
+        }, function finalizerThree (context, callback) {
+          x += 1;
+          x.should.equal(5);
+          callback(undefined, context);
+        }))({}, {write:_.constant(), end:_.constant()});
   });
 
 });
